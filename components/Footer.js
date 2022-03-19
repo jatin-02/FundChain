84cebfdb5a0cb06957ba6ff6ac52ffce5a521d05
Jatin Soni <jatinsoni1202@gmail.com>
2022-03-19 12:09:19 +0530

    repo initialise

---
+++ components/Footer.js
@@ -0,0 +1,109 @@
+import {
+  Box,
+  chakra,
+  Container,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+  Heading,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whicyanpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whicyanpha.200"),
+      }}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        spacing={4}
+        justify={"center"}
+        align={"center"}
+      >
+        <Heading
+          textAlign={useBreakpointValue({ base: "center", md: "left" })}
+          fontFamily={"heading"}
+          color={useColorModeValue("cyan.800", "white")}
+          as="h2"
+          size="lg"
+        >
+          <Box
+            className="gradient"
+            as={"span"}
+            color={useColorModeValue("cyan.400", "cyan.300")}
+            position={"relative"}
+            zIndex={10}
+            _after={{
+              content: '""',
+              position: "absolute",
+              left: 0,
+              bottom: 0,
+              w: "full",
+              h: "30%",
+              zIndex: -1,
+            }}
+          >
+            <NextLink href="/">FundChain</NextLink>
+          </Box>
+        </Heading>
+        <Stack direction={"row"} spacing={6}>
+          <NextLink href="/">Home</NextLink>
+          <Link href={"https://github.com/jatin-02"} isExternal>
+            Github
+          </Link>
+          <Link href={"jatinsoni1202@gmail.com"} isExternal>
+            Contact
+          </Link>
+        </Stack>
+      </Container>
+
+      <Box
+        borderTopWidth={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Container
+          as={Stack}
+          maxW={"6xl"}
+          py={4}
+          spacing={4}
+          textAlign={"center"}
+        >
+          <Text>Build by Dhruvil, Hemant , Prince & Jatin</Text>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
